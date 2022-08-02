f40b570e7d7be4f0bf9de0dfce00e8d37e685a64
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-08-02 19:47:55 -0300

    add some commands

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const qrcode = require("qrcode-terminal");
 const fs = require("fs");
 const { Client, LocalAuth, MessageMedia } = require("whatsapp-web.js");
+
 const client = new Client({
   authStrategy: new LocalAuth(),
   puppeteer: {
@@ -16,60 +17,20 @@ client.on("ready", () => {
   console.log("Client is ready!");
 });
 
-async function createUserRepository(user) {
-  const usersRepository = await loadUsersRepository();
-  const data = [...usersRepository, user];
-  fs.writeFileSync("./db.json", JSON.stringify(data));
-  return user.id;
-}
-
-async function loadUsersRepository() {
-  const data = await fs.readFileSync("./db.json", "utf8");
-  const users = JSON.parse(data);
-  return users;
-}
-
-async function loadUsers() {
-  const data = await fs.readFileSync("./db.json", "utf8");
-  return data;
-}
-
 client.on("message", async (msg) => {
-  //   if (msg.body == "!entra") {
-  //     const Users = await loadUsersRepository();
-  //     const contact = await msg.getContact();
-  //     const chat = await msg.getChat();
-  //     const user = contact.id.user;
-  //     console.log(user);
-  //     console.log(user);
-  //     const newUser = {
-  //       id: contact.id.user,
-  //     };
-
-  //     const savedUser = await createUserRepository(newUser);
-  //     // fs.writeFileSync("./db.json", [
-  //     //   ...users,
-  //     //   JSON.parse(JSON.stringify(contact.id.user)),
-  //     // ]);
-  //     await chat.sendMessage(`Bem vindo ${savedUser}`);
-  //   }
-
-  //   if (msg.body == "!users") {
-  //     const chat = await msg.getChat();
-  //     const Users = await loadUsers();
-  //     await chat.sendMessage(`${Users}`);
-  //   }
-
   if (msg.body == "!salve") {
     const chat = await msg.getChat();
-    const contact = await msg.getContact();
     await chat.sendMessage(`Salve mlkote`);
   }
 
   if (msg.body == "!fut") {
     const chat = await msg.getChat();
-    const contact = await msg.getContact();
     await chat.sendMessage(`é hoje que o pilas toma uma canetinha`);
+  }
+
+  if (msg.body == "!foto") {
+    const chat = await msg.getChat();
+    await chat.sendMessage(`sem foto sem ponto`);
   }
 });
 
