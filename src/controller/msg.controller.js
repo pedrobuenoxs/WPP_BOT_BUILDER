84b84679ab0762aa5d24e1d89920f7752c6f5abb
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-08-03 20:44:18 -0300

    add helper to controller

--- src/controller/msg.controller.js
+++ src/controller/msg.controller.js
@@ -1,9 +1,16 @@
+const { simpleCommand, appCommand } = require("../helper/is-command.helper");
 module.exports = class MsgController {
-  constructor(service) {
-    this.service = service;
+  constructor(sc_service, app) {
+    this.sc.service = sc_service;
+    this.app = app;
   }
 
   async handle(msg) {
-    await this.service.handle(msg);
+    if (simpleCommand(msg)) {
+      await this.sc.service.handle(msg);
+    }
+    if (appCommand(msg)) {
+      await this.app.handle(msg);
+    }
   }
 };
