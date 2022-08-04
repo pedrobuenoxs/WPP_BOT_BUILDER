5ee7b5fc8d68c581ec1f1558a419c2ccac236c30
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-08-03 21:29:55 -0300

    remote blank space to msgs

--- src/service/app.service.js
+++ src/service/app.service.js
@@ -4,43 +4,31 @@ module.exports = class App {
     const command = commandArray(msg)[0];
 
     if (command == "!help") {
-      await chat.sendMessage(`
-      !entrar - Entra no ranking
+      await chat.sendMessage(`!entrar - Entra no ranking
       !editar - Edita o seu nome
       !sair - Sai do ranking [vai perder os pontos bobão]
       !ranking - Ranking atualizado
-      !ban - Bane um usuário do grupo
-      `);
+      !ban - Bane um usuário do grupo`);
     }
 
     if (command == "!entrar") {
-      await chat.sendMessage(`
-        alá o financiador da h7!
-        `);
+      await chat.sendMessage(`alá o financiador da h7!`);
     }
 
     if (command == "!editar") {
-      await chat.sendMessage(`
-            decida-se, amigo!
-            `);
+      await chat.sendMessage(`decida-se, amigo!`);
     }
 
     if (command == "!sair") {
-      await chat.sendMessage(`
-                :(
-                `);
+      await chat.sendMessage(`:(`);
     }
 
     if (command == "!ranking") {
-      await chat.sendMessage(`
-                    calmô!
-                    `);
+      await chat.sendMessage(`calmô!`);
     }
 
     if (command == "!ban") {
-      await chat.sendMessage(`
-                        durdo ou bis?
-                        `);
+      await chat.sendMessage(`durdo ou bis?`);
     }
   }
 };
