a82d56316f864e3cf41996349bf0d36fd771ccac
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-08-07 14:05:07 -0300

    entrar ir done

--- src/service/app.service.js
+++ src/service/app.service.js
@@ -1,51 +1,16 @@
-const { GroupChat } = require("whatsapp-web.js");
+const UserRepository = require("../db/user.repository");
+const repository = new UserRepository();
+const Ranking = require("../validator/ranking.validator");
 
 module.exports = class App {
   async handle(msg, chat, autor) {
+    const ranking = new Ranking(msg, chat, autor, repository);
     const strToArray = (cmdString) => cmdString.split(" ");
     const commandArray = strToArray(msg);
     const command = commandArray[0];
 
-    if (command == "!help") {
-      await chat.sendMessage(
-        `!entrar - Entra no ranking\n!editar - Edita o seu nome\n!sair - Sai do ranking [vai perder os pontos bobão]\n!ranking - Ranking atualizado\n!ban - Bane um usuário do grupo`
-      );
-    }
-
-    if (command == "!titulo") {
-      try {
-        if (autor == "5519995790911@c.us") {
-          const auxTitle = commandArray.slice(1, commandArray.length).join(" ");
-          await chat.setSubject(auxTitle);
-        } else {
-          await chat.sendMessage("você não bobão");
-        }
-      } catch (err) {
-        console.log(err);
-      }
-    }
-
     if (command == "!entrar") {
-      await chat.sendMessage(`alá o financiador da h7!`);
-    }
-
-    if (command == "!editar") {
-      await chat.sendMessage(`decida-se, amigo!`);
-    }
-
-    if (command == "!sair") {
-      await chat.sendMessage(`:(`);
-    }
-
-    if (command == "!ranking") {
-      await chat.sendMessage(`calmô!`);
-    }
-
-    if (command == "!ban") {
-      await chat.sendMessage(`durdo ou bis?`);
-    }
-    if (command == "!pontuar") {
-      await chat.sendMessage(`beach tennis não vale em`);
+      await ranking.register();
     }
   }
 };
